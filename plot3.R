15c29781a98fa810a141086fa4f2a85fcf4c8a61
Rene Broekhoven <renebroekhoven@gmail.com>
2015-08-09 14:14:39 +0200

    final final

--- plot3.R
+++ plot3.R
@@ -37,9 +37,9 @@ points(mydf$DateTime, mydf$Sub_metering_3,
      type = "l",
      col = "blue")
 
-legend("topright", legend  = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       text.col = c(1,2,4),
+legend("topright", legend  = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        col = c(1,2,4),
+       text.col = c(1,1,1),
        lty = c(1,1,1), 
        pch = c(NA, NA, NA) )
 
--- plot4.R
+++ plot4.R
@@ -51,8 +51,8 @@ points(mydf$DateTime, mydf$Sub_metering_3,
        col = "blue")
 
 legend("topright", legend  = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       text.col = c(1,2,4),
        col = c(1,2,4),
+       text.col = c(1,1,1),
        lty = c(1,1,1), 
        pch = c(NA, NA, NA) )
 
